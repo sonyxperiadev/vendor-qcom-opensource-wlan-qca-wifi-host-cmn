e1361573affde9d89f0573ccd43acde0779e7e38
Pavel Dubrova <pashadubrova@gmail.com>
2024-11-28 03:20:56 +0200

    nlink: Remove inline specifier from nl80211hdr_put()
    Keep nl80211hdr_put() only static, remove from the header
    because this function is not used outside and move it
    above so that the function is available for calling.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- utils/nlink/inc/wlan_nlink_srv.h
+++ utils/nlink/inc/wlan_nlink_srv.h
@@ -55,9 +55,9 @@ void cld80211_oem_send_reply(struct sk_buff *msg, void *hdr,
 				    struct nlattr *nest, int flags);
 
 /**
- * nl80211hdr_put() - API to allocate skb for cld80211 msg
- * @hdr: nl80211hdr pointer
+ * cld80211_oem_rsp_alloc_skb() - API to allocate skb for cld80211 msg
  * @portid: Port ID
+ * @hdr: nl80211hdr pointer
  * @nest: pointer of vendor nested attribute
  * @flags: Flags
  *
@@ -65,7 +65,6 @@ void cld80211_oem_send_reply(struct sk_buff *msg, void *hdr,
  *
  * Return: Pointer to skbuff
  */
-
 struct sk_buff *
 cld80211_oem_rsp_alloc_skb(uint32_t portid, void **hdr, struct nlattr **nest,
 			   int *flags);
@@ -79,21 +78,6 @@ int nl_srv_unregister(tWlanNlModTypes msg_type,
 int nl_srv_ucast(struct sk_buff *skb, int dst_pid, int flag,
 			int app_id, int mcgroup_id);
 int nl_srv_bcast(struct sk_buff *skb, int mcgroup_id, int app_id);
-
-/**
- * nl80211hdr_put() - API to fill genlmsg header
- * @skb: Sk buffer
- * @portid: Port ID
- * @seq: Sequence number
- * @flags: Flags
- * @cmd: Command id
- *
- * API to fill genl message header for brodcast events to user space
- *
- * Return: Pointer to user specific header/payload
- */
-static inline void *nl80211hdr_put(struct sk_buff *skb, uint32_t portid,
-		     uint32_t seq, int flags, uint8_t cmd);
 #else
 int nl_srv_ucast(struct sk_buff *skb, int dst_pid, int flag);
 int nl_srv_bcast(struct sk_buff *skb);
